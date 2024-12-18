a1f216dd0e8f8a582ae38531fe9aec20b292601b
GregBaughDev <gregbaughdesign@outlook.com>
2024-12-18 20:09:34 +1100

    FInish minheap

--- tree/minheap.cpp
+++ tree/minheap.cpp
@@ -11,9 +11,9 @@ class MinHeap
 {
 public:
     MinHeap() : length{0}, items{} {};
-    int peek();
+    std::optional<int> peek();
     void push(int value);
-    int pop();
+    std::optional<int> pop();
     int getLength();
 
 private:
@@ -26,8 +26,13 @@ private:
     void heapifyUp(int index);
 };
 
-int MinHeap::pop()
+std::optional<int> MinHeap::pop()
 {
+    if (length == 0)
+    {
+        return std::nullopt;
+    }
+
     int item = items.front();
     length--;
     items.at(0) = items.at(length);
@@ -122,9 +127,16 @@ void MinHeap::heapifyDown(int index)
     }
 }
 
-int MinHeap::peek()
+std::optional<int> MinHeap::peek()
 {
-    return items[0];
+    if (length == 0)
+    {
+        return std::nullopt;
+    }
+    else
+    {
+        return items.at(0);
+    }
 }
 
 int MinHeap::getLeftIndex(int index)
@@ -170,12 +182,116 @@ TEST_CASE("MinHeap test 1")
     CHECK(mh.pop() == 9);
     CHECK(mh.pop() == 11);
     CHECK(mh.pop() == 20);
+    CHECK(mh.peek() == 69);
     CHECK(mh.pop() == 69);
     CHECK(mh.pop() == 100);
     CHECK(mh.pop() == 100);
     CHECK(mh.pop() == 360);
     CHECK(mh.pop() == 365);
+    CHECK(mh.peek() == 420);
     CHECK(mh.pop() == 420);
     CHECK(mh.pop() == 864);
     CHECK(mh.pop() == 7777);
+    CHECK(mh.pop() == std::nullopt);
+}
+
+TEST_CASE("MinHeap test 2")
+{
+    MinHeap mh;
+    mh.push(1002);
+    mh.push(2);
+    mh.push(2);
+    mh.push(2);
+    mh.push(100);
+    mh.push(199);
+    mh.push(90);
+    mh.push(4);
+    mh.push(765);
+    mh.push(99804);
+    mh.push(1000030);
+    mh.push(78);
+    mh.push(10);
+    mh.push(20);
+    mh.push(23);
+    mh.push(45);
+    mh.push(764);
+    mh.push(5303);
+    mh.push(303);
+    mh.push(808);
+    mh.push(1909);
+    mh.push(2233);
+    mh.push(1111);
+    mh.push(1); 
+    mh.push(2); 
+    mh.push(3);
+    mh.push(2001);
+    mh.push(1999);
+    mh.push(7930);
+    mh.push(90);
+    mh.push(37);
+    mh.push(52);
+    mh.push(2025);
+ 
+    CHECK(mh.getLength() == 33);
+    CHECK(mh.peek() == 1);
+    CHECK(mh.pop() == 1);
+    CHECK(mh.pop() == 2);
+    CHECK(mh.pop() == 2);
+    CHECK(mh.pop() == 2);
+    CHECK(mh.pop() == 2);
+    CHECK(mh.pop() == 3);
+    CHECK(mh.pop() == 4);
+    CHECK(mh.pop() == 10);
+
+    CHECK(mh.peek() == 20);
+    CHECK(mh.pop() == 20);
+    CHECK(mh.pop() == 23);
+    CHECK(mh.pop() == 37);
+    CHECK(mh.getLength() == 22);
+    CHECK(mh.pop() == 45);
+    CHECK(mh.pop() == 52);
+    CHECK(mh.pop() == 78);
+    CHECK(mh.peek() == 90);
+    CHECK(mh.pop() == 90);
+    CHECK(mh.pop() == 90);
+    CHECK(mh.pop() == 100);
+    CHECK(mh.pop() == 199);
+    CHECK(mh.peek() == 303);
+    CHECK(mh.pop() == 303);
+    CHECK(mh.pop() == 764);
+    CHECK(mh.pop() == 765);
+    CHECK(mh.pop() == 808);
+
+    mh.push(13);
+
+    CHECK(mh.peek() == 13);
+    CHECK(mh.pop() == 13);
+    CHECK(mh.pop() == 1002);
+    CHECK(mh.pop() == 1111);
+    CHECK(mh.pop() == 1909);
+
+    mh.push(9999);
+
+    CHECK(mh.peek() == 1999);
+    CHECK(mh.pop() == 1999);
+    CHECK(mh.peek() == 2001);
+    CHECK(mh.pop() == 2001);
+    CHECK(mh.pop() == 2025);
+
+    mh.push(2234);
+
+    CHECK(mh.peek() == 2233);
+    CHECK(mh.pop() == 2233);
+    CHECK(mh.pop() == 2234);
+    CHECK(mh.pop() == 5303);
+    CHECK(mh.getLength() == 4);
+    CHECK(mh.peek() == 7930);
+    CHECK(mh.pop() == 7930);
+    CHECK(mh.peek() == 9999);
+    CHECK(mh.pop() == 9999);
+    CHECK(mh.pop() == 99804);
+    CHECK(mh.pop() == 1000030);
+    CHECK(mh.getLength() == 0);
+    CHECK(mh.peek() == std::nullopt);
+    CHECK(mh.pop() == std::nullopt);
 }
